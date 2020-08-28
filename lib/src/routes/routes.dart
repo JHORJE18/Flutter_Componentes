e6f6fb99f37ee7c9547127b6a8e6faea8addcdce
Jorge López Gil <wiijlg@hotmail.com>
2020-08-28 17:02:24 +0200

    Container & AnimatedContainers

--- lib/src/routes/routes.dart
+++ lib/src/routes/routes.dart
@@ -1,4 +1,5 @@
 import 'package:componentes/src/pages/alert_page.dart';
+import 'package:componentes/src/pages/animated_container_page.dart';
 import 'package:componentes/src/pages/avatar_page.dart';
 import 'package:componentes/src/pages/card_page.dart';
 import 'package:componentes/src/pages/home_page.dart';
@@ -9,7 +10,8 @@ Map<String, WidgetBuilder> getApplicationRoutes() {
     '/': (BuildContext context) => HomePage(),
     'alert': (BuildContext context) => AlertPage(),
     'avatar': (BuildContext context) => AvatarPage(),
-    'card': (BuildContext context) => CardPage()
+    'card': (BuildContext context) => CardPage(),
+    'animated': (BuildContext context) => AnimatedContainerPage()
   };
 }
 
