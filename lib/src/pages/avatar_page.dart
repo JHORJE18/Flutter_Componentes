020c3a73aed87b287a720a645986f937385c1a77
Jorge López Gil <wiijlg@hotmail.com>
2020-08-28 16:23:17 +0200

    Avatar image

--- lib/src/pages/avatar_page.dart
+++ lib/src/pages/avatar_page.dart
@@ -6,6 +6,30 @@ class AvatarPage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text('Avatar Page 😱'),
+        actions: [
+          Container(
+            padding: EdgeInsets.all(5.0),
+            child: CircleAvatar(
+              backgroundImage: NetworkImage(
+                  'https://api.adorable.io/avatars/55/abott@adorable.png'),
+              radius: 25.0,
+            ),
+          ),
+          Container(
+            padding: EdgeInsets.all(5.0),
+            child: CircleAvatar(
+              child: Text('JLG'),
+              backgroundColor: Colors.amberAccent,
+            ),
+          )
+        ],
+      ),
+      body: Center(
+        child: FadeInImage(
+          image: NetworkImage(
+              'https://cdnb.artstation.com/p/assets/images/images/015/803/707/large/melissa-hachid-tardis-retake-final-wallpaper.jpg'),
+          placeholder: AssetImage('assets/jar-loading.gif'),
+        ),
       ),
     );
   }
